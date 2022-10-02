d370b6331800532e0a5418998284647948a2ac15
Fay Spaniel <Fay.Spaniel@yandex.ru>
2022-10-03 01:37:29 +0300

    a

---
+++ app/src/main/java/com/example/cinema2/Wednesday.java
@@ -0,0 +1,14 @@
+package com.example.cinema2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Wednesday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wednesday);
+    }
+}
